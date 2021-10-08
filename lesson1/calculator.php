893dcc5f8159a83ef9ab0caa6c7f4fe7e29ca2c3
mytran123 <mytran091196@gmai.com>
2021-10-08 17:37:41 +0700

    bài tập

---
+++ lesson1/calculator.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<form action="" method="post">
+    <div class="">
+        <h1>Simple Calculator</h1>
+        <input type="number" name="first_operand" size="30"  placeholder="First operand" />
+        <input type="hidden" name="operator" size="30" placeholder="Operator" />
+        <input type="number" name="second_operand" size="30" placeholder="Second operand" />
+        <button type="submit">Calculate</button>
+    </div>
+</form>
+</body>
+</html>
+<?php
