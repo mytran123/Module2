2f2062195e766e0edffbc6bd0ed65a52d6240ffc
mytran123 <mytran091196@gmai.com>
2021-10-08 17:57:43 +0700

    bài tập

---
+++ lesson2/TH_layout/nav.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+<ul>
+    <li><a href="#">London</a></li>
+    <li><a href="#">Paris</a></li>
+    <li><a href="#">Tokyo</a></li>
+</ul>
+</body>
+</html>
+
