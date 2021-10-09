4b68bf90d46f9f95694f7169d068c7fdf6c6d0bb
mytran123 <mytran091196@gmai.com>
2021-10-09 12:58:11 +0700

    bài tập

--- lesson1/calculator.php
+++ lesson1/calculator.php
@@ -9,7 +9,14 @@
     <div class="">
         <h1>Simple Calculator</h1>
         <input type="number" name="first_operand" size="30"  placeholder="First operand" />
-        <input type="hidden" name="operator" size="30" placeholder="Operator" />
+        <input type="text">
+        <select name="operator">
+            <option name="choose" value="add">+</option>
+            <option name="choose" value="subtract">-</option>
+            <option name="choose" value="times">*</option>
+            <option name="choose" value="division">/</option>
+        </select>
+<!--        <input type="select" name="operator" size="30" placeholder="Operator" />-->
         <input type="number" name="second_operand" size="30" placeholder="Second operand" />
         <button type="submit">Calculate</button>
     </div>
@@ -17,3 +24,17 @@
 </body>
 </html>
 <?php
+    $sothunhat = $_POST["first_operand"];
+    $sothuhai = $_POST["second_operand"];
+    if($_POST['choose'] == add) {
+    echo "$sothunhat + $sothuhai";
+    }
+    else if($_POST['choose'] == subtract) {
+    echo "$sothunhat - $sothuhai";
+    }
+    else if($_POST['choose'] == times) {
+    echo "$sothunhat * $sothuhai";
+    }
+    else echo "$sothunhat / $sothuhai";
+    }
+?>
---
+++ lesson2/TH_layout/footer.php
@@ -0,0 +1,2 @@
+Copyright &copy; CodeGym.vn
+<link rel="stylesheet" href="style3.css">
