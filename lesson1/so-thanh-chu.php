b79f1e51414641232641108eb86913351c49a47f
mytran123 <mytran091196@gmai.com>
2021-10-11 04:46:11 +0700

    bài tập

--- lesson1/so-thanh-chu.php
+++ lesson1/so-thanh-chu.php
@@ -1,56 +1,60 @@
-
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="get">
+    <input type="text" name="number">
+    <button>Submit</button>
+</form>
+</body>
+</html>
 <?php
-function convert_number_to_words($number) {
-    $dictionary = array(
-        0 => 'không',
-        1 => 'một',
-        2 => 'hai',
-        3 => 'ba',
-        4 => 'bốn',
-        5 => 'năm',
-        6 => 'sáu',
-        7 => 'bảy',
-        8 => 'tám',
-        9 => 'chín',
-        10 => 'mười',
-        11 => 'mười một',
-        12 => 'mười hai',
-        13 => 'mười ba',
-        14 => 'mười bốn',
-        15 => 'mười năm',
-        16 => 'mười sáu',
-        17 => 'mười bảy',
-        18 => 'mười tám',
-        19 => 'mười chín',
-        20 => 'hai mươi',
-        30 => 'ba mươi',
-        40 => 'bốn mươi',
-        50 => 'năm mươi',
-        60 => 'sáu mươi',
-        70 => 'bảy mươi',
-        80 => 'tám mươi',
-        90 => 'chín mươi',
-        100 => 'một trăm',
-        200 => 'hai trăm',
-        300 => 'ba trăm',
-        400 => 'bốn trăm',
-        500 => 'năm trăm',
-        600 => 'sáu trăm',
-        700 => 'bảy trăm',
-        800 => 'tám trăm',
-        900 => 'chín trăm'
-    );
-    if (!is_numeric($number)) {
-        return false;
-    }
-    switch (true) {
-        case $number < 10 && $number >= 0;
-            echo $dictionary[$number];
-            break;
-        default:
-            echo "out of ability";
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if (isset($_GET["number"])) {
+        readNumber($_GET["number"]);
     }
 }
+
+function readNumber($number) {
+    if ($number < 10) {
+        echo numberOneDigit($number);
+    } else if ($number < 100) {
+        echo numberTwoDigit($number);
+    }
+}
+
+function numberOneDigit($number) {
+    $arr = ["zero","one","two","three","four","five","six","seven","eight","nine","ten"];
+    return $arr[$number];
+}
+
+function numberTwoDigit($number) {
+    $teen = [10 => "ten",11 => "eleven",12 => "twelve",13 => "thirteen",14 => "fourteen",15 => "fifteen",16 => "sixteen",17 => "seventeen",18 => "eighteen",19 => "nineteen"];
+    $ty = [2 => "twenty",3 => "thirty",4 => "forty",5 => "fifty",6 => "sixty",7 => "seventy",8 => "eighty",9 => "ninety"];
+    if ($number < 20) {
+        return $teen[$number];
+    } else if ($number % 10 == 0) {
+        return $ty[$number[0]];
+    } else {
+        return $ty[$number[0]]." ".numberOneDigit($number[1]);
+    }
+}
+
+function numberThreeDigit($number) {
+    $ar = [1 => "one hundred",2 => "two hundred",3 => "three hundred",4 => "four hundred",5 => "five hundred",6 => "six hundred",7 => "seven hundred",8 => "eight hundred",9 => "nine hundred"];
+    if ($number % 100) {
+        return $ar[$number[0]];
+    } else if ($number) {
+
+    }
+}
+
 
 
 
